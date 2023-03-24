53ce050e21d6da6346cf28f0930bee6c9008456e
Gaurav <somaigaurav@gmail.com>
2023-03-24 15:32:57 +0530

    sign in implemented

--- app/src/main/java/com/example/firebaseauthlearn/LoginActivity.kt
+++ app/src/main/java/com/example/firebaseauthlearn/LoginActivity.kt
@@ -1,11 +1,40 @@
 package com.example.firebaseauthlearn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.firebaseauthlearn.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.login.setOnClickListener{
+            val email = binding.signinemail.text.toString()
+            val pass = binding.signinpassword.text.toString()
+            if(email.isNotEmpty() && pass.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            }
+        }
+
+
     }
 }
